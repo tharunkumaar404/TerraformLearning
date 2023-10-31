4c3f24205e8fed3ba691c5ee3a57ec9bca50001e
Tharun Kumaar <tharunkumaar2000@gmail.com>
2023-10-31 15:33:00 +0530

    Update test.tf

--- .terraform/test.tf
+++ .terraform/test.tf
@@ -1,1 +1,9 @@
+provider "aws" {
+  region     = "ap-south-1"
+  access_key = ""
+  secret_key = ""
 
+resource "aws_instance" "my-ec2-auto" {
+    ami = "ami-05c0f5389589545b7"
+    instance_type = "t2.micro"
+}
